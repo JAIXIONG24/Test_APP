b5794c496f553359cc5687a7c28c4d46313a9d8f
i18102 <61220264+i18102@users.noreply.github.com>
2020-08-31 13:40:39 +0900

    Add files via upload

--- test.php
+++ test.php
@@ -2,31 +2,35 @@
 
 if($_SERVER["REQUEST_METHOD"]=="POST"){
 
-	$username = $_POST["username"];
-	$email = $_POST["email"];
-	$password = $_POST["password"];
+	$username = $_GET["username"];
+	$email = $_GET["email"];
+	$password = $_GET["password"];
 
 	require_once 'init.php';
 
 
 	$query = " insert into users(username,email,password) values('$username','$email','$password')";
   $stmt = $dbh->query($query);
-	$rec = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	//$rec = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-	if($rec){
+
+	if($stmt){
+
 		$result["success"] = "1";
 		$result["message"] = "success";
 
 		echo json_encode($result);
+		mysqli_close($dbh);
 
 	}else{
 		$result["success"] = "0";
 		$result["message"] = "error";
 
 		echo json_encode($result);
+		mysqli_close($dbh);
 
 	}
-	
+
 }
 
 
